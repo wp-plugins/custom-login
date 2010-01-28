cf3d759f35076ba00a4b922dc79c82af8e7ee535
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-28 21:44:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-login/trunk@199134 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-login.php
+++ custom-login.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Plugin Name: Custom Login
- * Plugin URI: http://wpcult.com/custom-login-plugin
+ * Plugin URI: http://austinpassy.com//wordpress-plugins/custom-login
  * Description: A simple way to customize your WordPress login screen! Use the built in and easy to use <a href="./options-general.php?page=custom-login.php">settings</a> page to do the work for you. So simple a caveboy can do it! Now featureing a HTML &amp; CSS box for advanced users. Sweet! Share you logins via the <a href="http://flickr.com/groups/custom-login/">Flickr</a> group!!
  * Version: 0.4.6.1
  * Author: Austin Passy
- * Author URI: http://austinpassy.com
+ * Author URI: http://frostywebdesigns.com
  *
  * @copyright 2009 - 2010
  * @author Austin Passy
