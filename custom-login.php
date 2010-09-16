6d7c2f415e7eca274ac3d23f9c4fec5ef75f44da
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-16 17:32:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-login/tags/0.7.3@290322 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-login.php
@@ -0,0 +1,357 @@
+<?php
+/*
+ * Plugin Name: Custom Login
+ * Plugin URI: http://austinpassy.com//wordpress-plugins/custom-login
+ * Description: A simple way to customize your WordPress login screen! Use the built in and easy to use <a href="./options-general.php?page=custom-login.php">settings</a> page to do the work for you. So simple a caveboy can do it! Now featuring a HTML &amp; CSS box for advanced users. Sweet! Share you logins via the <a href="http://flickr.com/groups/custom-login/">Flickr</a> group!! <a href="../wp-content/plugins/custom-login/uninstall.php" title="Uninstall the Custom Login plugin with this script">Uninstall script</a>
+ * Version: 0.8.development
+ * Author: Austin Passy
+ * Author URI: http://frostywebdesigns.com
+ *
+ * @copyright 2009 - 2010
+ * @author Austin Passy
+ * @link http://frostywebdesigns.com/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @package CustomLogin
+ */
+
+/**
+ * Version checker
+ * @since 0.7
+ */
+if ( !function_exists( 'version_check' ) ) {
+	function version_check() {		
+		global $wp_version;
+	
+		if ( version_compare( $wp_version, '3.0', '<' ) ) {
+			exit("This plugin requires WordPress 3.0 or greater.");
+		}		
+	}		
+}
+
+/**
+ * Make sure we get the correct directory.
+ * @since 0.3
+ */
+	if ( !defined( 'WP_CONTENT_URL' ) )
+		define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+	if ( !defined( 'WP_CONTENT_DIR' ) )
+		define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+	if ( !defined( 'WP_PLUGIN_URL' ) )
+		define('WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+	if ( !defined( 'WP_PLUGIN_DIR' ) )
+		define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+
+/**
+ * Define constant paths to the plugin folder.
+ * @since 0.3
+ */
+	define( CUSTOM_LOGIN, WP_PLUGIN_DIR . '/custom-login' );
+	define( CUSTOM_LOGIN_URL, WP_PLUGIN_URL . '/custom-login' );
+	
+	define( CUSTOM_LOGIN_ADMIN, WP_PLUGIN_DIR . '/custom-login/library/admin' );
+	define( CUSTOM_LOGIN_CSS, WP_PLUGIN_URL . '/custom-login/library/css' );
+	define( CUSTOM_LOGIN_JS, WP_PLUGIN_URL . '/custom-login/library/js' );
+
+/**
+ * Add the settings page to the admin menu.
+ * @since 0.3
+ */
+	//add_action( 'init', 'custom_login_admin_warnings' );
+	add_action( 'admin_init', 'custom_login_admin_init' );
+	add_action( 'admin_menu', 'custom_login_add_pages' );
+	add_action( 'login_head', 'custom_login' );
+	add_action( 'login_head', 'custom_login_html' );
+	
+/**
+ * Add jquery if using custom html
+ * @since 0.4.6
+ */
+	add_action( 'login_head', 'custom_login_jquery', 1 );
+
+/**
+ * Filters.
+ * @since 0.3.3
+ */	
+	add_filter( 'plugin_action_links', 'custom_login_plugin_actions', 10, 2 ); //Add a settings page to the plugin menu
+
+/**
+ * Load the RSS Shortcode settings if in the WP admin.
+ * @since 0.1
+ */
+	if ( is_admin() )
+		require_once( CUSTOM_LOGIN_ADMIN . '/settings-admin.php' );
+		require_once( CUSTOM_LOGIN_ADMIN . '/dashboard.php' );
+
+/**
+ * Load the settings from the database.
+ * @since 0.3
+ */
+	$custom_login = get_option( 'custom_login_settings' );
+
+ /**
+ * Load the stylesheet
+ * @since 0.4
+ */   
+function custom_login_admin_init() {
+	wp_register_style( 'farbtastic', CUSTOM_LOGIN_CSS . '/farbtastic.css' );
+	wp_register_style( 'custom-login-tabs', CUSTOM_LOGIN_CSS . '/tabs.css' );
+	wp_register_style( 'custom-login-admin', CUSTOM_LOGIN_CSS . '/custom-login-admin.css' );
+}
+
+/**
+ * Function to add the settings page
+ * @since 0.3
+ * @modified 0.4
+ */
+function custom_login_add_pages() {
+	if ( function_exists( 'add_options_page' ) ) 
+		$page = add_options_page( 'Custom Login Settings', 'Custom Login', 10, 'custom-login.php', custom_login_page );
+			add_action( 'admin_print_styles-' . $page, 'custom_login_admin_style' );
+			add_action( 'admin_print_scripts-' . $page, 'custom_login_admin_script' );
+}
+
+/**
+ * Function to add the style to the settings page
+ * @since 0.4
+ * @modified 0.8
+ */
+function custom_login_admin_style() {
+	wp_enqueue_style( 'thickbox' );
+	wp_enqueue_style( 'custom-login-tabs' );
+	wp_enqueue_style( 'custom-login-admin' );
+}
+
+/**
+ * Function to add the script to the settings page
+ * @since 0.4
+ * @modified 0.7
+ * @modified 0.8
+ */
+function custom_login_admin_script() {
+	wp_enqueue_script( 'common' );
+	wp_enqueue_script( 'wp-lists' );
+	wp_enqueue_script( 'postbox' );
+	wp_enqueue_script( 'thickbox' );
+	wp_enqueue_script( 'theme-preview' );
+	wp_enqueue_script( 'autoresize', CUSTOM_LOGIN_JS . '/autoresize.min.js', array( 'jquery' ), '1.04', false );
+	wp_enqueue_script( 'custom-login', CUSTOM_LOGIN_JS . '/custom-login.js', array( 'jquery' ), '0.1', false );
+	wp_enqueue_script( 'jscolor', CUSTOM_LOGIN_JS . '/jscolor.js', false, '1.3.1', false );
+}
+
+
+/**
+ * Add stylesheet to the login head
+ * @since 0.3
+ */
+function custom_login() {
+	global $custom_login;
+	
+	if ( $custom_login[ 'use_custom' ] != true ) :
+		
+		echo '<!-- Start Custom Login by Austin Passy -->' . "\n\n";
+		echo '<link rel="stylesheet" type="text/css" href="' . CUSTOM_LOGIN_CSS . '/custom-login.css" />' . "\n\n";
+		
+			echo '<script type="text/javascript">' . "\n";
+			echo 'jQuery(document).ready(' . "\n";
+			echo '	function() {' . "\n";
+			
+			echo '		jQuery(\'html body.login h1 a\').removeAttr(\'href\').attr(\'href\',\'' . get_bloginfo('url') . '\');' . "\n";
+			
+			//echo '		jQuery(\'html body.login p#nav\').append(\'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7431290" title="Like this plugin? Please help me out, and donate">Dontate $1</a>\');' . "\n";
+			
+			echo '		jQuery(\'html body.login p#backtoblog\').append(\'<span class="byauthor"><a href="http://austinpassy.com/wordpress-plugin/custom-login/" title="Custom Login">Custom Login</a> by <a href="http://austinpassy.com/" title="Austin Passy">Austin Passy</a></span>\');' . "\n";
+			
+			echo '	}' . "\n";
+			echo ');' . "\n";
+			echo '</script>' . "\n";			
+		
+		echo '<!-- End Custom Login by Austin Passy - @link: http://austinpassy.com -->' . "\n\n";
+	
+	else :
+	
+		echo '<!-- Start Custom Login by Austin Passy -->' . "\n\n";
+		
+		echo '<style type="text/css">' . "\n";
+		
+		/* Custom user CSS - hand coded */
+		echo $custom_login[ 'cl_login_custom_code' ] . "\n\n";
+		/* End custom */
+		
+		echo 'html {' . "\n";
+		/* html background */
+		if ( $custom_login[ 'cl_html_background_color' ] != '' ) 
+			echo 'background:' . $custom_login[ 'cl_html_background_color' ] . ' url( \'' . $custom_login[ 'cl_html_background_url' ] . '\' ) left top '. $custom_login[ 'cl_html_background_repeat' ] . '; }' . "\n\n";
+		else 
+			echo 'background: transparent url( \'' . $custom_login[ 'cl_html_background_url' ] . '\' ) left top '. $custom_login[ 'cl_html_background_repeat' ] . '; }' . "\n\n";
+		
+		if ( $custom_login[ 'cl_html_border_top_color' ] != '' && $version3 == 'false' ) :
+			echo 'body.login {' . "\n"; 
+			echo 'border-top-color:' . $custom_login[ 'cl_html_border_top_color' ] . '; }' . "\n\n";
+		endif;
+		
+		if ( $custom_login[ 'cl_html_border_top_background' ] != '' && $version3 == 'true' ) :
+			echo 'body.login {' . "\n"; 
+			echo 'background: transparent url( \'' . $custom_login[ 'cl_html_border_top_background' ] . '\' ) left top repeat-x; }' . "\n\n";
+		endif;
+		
+		echo '/* Diplays the custom graphics for the login screen*/' . "\n";
+		echo '#login form {' . "\n";
+		
+		if ( $custom_login[ 'cl_login_form_background_color' ] != '' ) 
+			echo 'background:' . $custom_login[ 'cl_login_form_background_color' ] . ' url( \'' . $custom_login[ 'cl_login_form_background' ] . '\' ) center top no-repeat;' . "\n";
+		else 
+			echo 'background: transparent url( \'' . $custom_login[ 'cl_login_form_background' ] . '\' ) center top no-repeat;' . "\n";
+		
+		echo 'padding-top:100px; }' . "\n\n";
+		
+		echo '/* Hides the default Wordpress Login content*/' . "\n";
+		echo '#login form {' . "\n";
+		/* border radius */
+		echo '-moz-border-radius: ' . $custom_login[ 'cl_login_form_border_radius' ] . 'px;' . "\n";
+		echo '-khtml-border-radius: ' . $custom_login[ 'cl_login_form_border_radius' ] . 'px;' . "\n";
+		echo '-webkit-border-radius: ' . $custom_login[ 'cl_login_form_border_radius' ] . 'px;' . "\n";
+		echo 'border-radius: ' . $custom_login[ 'cl_login_form_border_radius' ] . 'px;' . "\n";
+		/* border */
+		echo 'border: ' . $custom_login[ 'cl_login_form_border' ] . 'px solid ' . $custom_login[ 'cl_login_form_border_color' ] . ';' . "\n";
+		/* box shadow */
+		echo '-moz-box-shadow: ' . $custom_login[ 'cl_login_form_box_shadow_1' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_2' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_3' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_4' ] . ';' . "\n";
+		echo '-webkit-box-shadow: ' . $custom_login[ 'cl_login_form_box_shadow_1' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_2' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_3' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_4' ] . ';' . "\n";
+		echo '-khtml-box-shadow: ' . $custom_login[ 'cl_login_form_box_shadow_1' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_2' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_3' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_4' ] . ';' . "\n";
+		echo 'box-shadow: ' . $custom_login[ 'cl_login_form_box_shadow_1' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_2' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_3' ] . 'px ' . $custom_login[ 'cl_login_form_box_shadow_4' ] . ';' . "\n";
+		echo '}' . "\n\n";
+		
+		echo '#login h1 {' . "\n";
+		echo 'display: none; }' . "\n";
+		
+		echo 'label {' . "\n";
+		echo 'color: ' . $custom_login[ 'cl_label_color' ] . ' !important; }' . "\n";
+		echo '</style>' . "\n\n";
+		
+		echo '<!-- End Custom Login by Austin Passy - @link: http://austinpassy.com -->' . "\n\n";
+	
+	endif;
+
+}
+
+ /**
+ * Load jQuery if "USE_custom_html_code" checkbox is checked
+ * @since 0.4.6
+ */   
+function custom_login_jquery() {
+	global $custom_login;
+	
+	if ( $custom_login[ 'cl_USE_custom_html_code' ] != false || $custom_login[ 'use_custom' ] != true ) 
+		echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js?ver=1.4.1"></script>' . "\n";
+}
+
+/**
+ * Add html after the body class
+ * @since 0.4.6
+ */
+function custom_login_html() {
+	global $custom_login;
+	
+	if ( $custom_login[ 'cl_USE_custom_html_code' ] != false ) :
+		echo '<script type="text/javascript">' . "\n";
+		echo 'jQuery(document).ready(' . "\n";
+		echo '	function() {' . "\n";
+				/* Custom user HTML - hand coded */
+		echo '		jQuery(\'html body.login\').append(\'' . $custom_login[ 'cl_login_custom_html_code' ] . '\');' . "\n";
+				/* End custom */
+		echo '	}' . "\n";
+		echo ');' . "\n";
+		echo '</script>' . "\n";
+	endif;
+
+}
+
+/**
+ * RSS WPCult Feed
+ * @since 0.3
+ * @package Admin
+ */
+if ( !function_exists( 'thefrosty_network_feed' ) ) :
+	function thefrosty_network_feed( $attr, $count ) {
+		
+		global $wpdb;
+		
+		include_once( ABSPATH . WPINC . '/rss.php' );
+		
+		$rss = fetch_rss( $attr );
+		
+		$items = array_slice( $rss->items, 0, '3' );
+		
+		echo '<div class="tab-content t' . $count . ' postbox open feed">';
+		
+		echo '<ul>';
+		
+		if ( empty( $items ) ) echo '<li>No items</li>';
+		
+		else
+		
+		foreach ( $items as $item ) : ?>
+		
+		<li>
+		
+		<a href='<?php echo $item[ 'link' ]; ?>' title='<?php echo $item[ 'description' ]; ?>'><?php echo $item[ 'title' ]; ?></a><br /> 
+		
+		<span style="font-size:10px; color:#aaa;"><?php echo date( 'F, j Y', strtotime( $item[ 'pubdate' ] ) ); ?></span>
+		
+		</li>
+		
+		<?php endforeach;
+		
+		echo '</ul>';
+		
+		echo '</div>';
+		
+	}
+endif;
+
+/**
+ * Plugin Action /Settings on plugins page
+ * @since 0.4.2
+ * @package plugin
+ */
+function custom_login_plugin_actions( $links, $file ) {
+ 	if( $file == 'custom-login/custom-login.php' && function_exists( "admin_url" ) ) {
+		$settings_link = '<a href="' . admin_url( 'options-general.php?page=custom-login.php' ) . '">' . __('Settings') . '</a>';
+		array_unshift( $links, $settings_link ); // before other links
+	}
+	return $links;
+}
+
+/**
+ * Warnings
+ * @since 0.5
+ * @package admin
+ */
+function custom_login_admin_warnings() {
+	global $custom_login;
+		
+		function custom_login_warning() {
+			global $custom_login;
+
+			if ( $custom_login[ 'use_custom' ] != true )
+				echo '<div id="custom-login-warning" class="updated fade"><p><strong>Custom Login plugin is not configured yet.</strong> It will use the defualt theme unless you configure the <a href="options-general.php?page=custom-login.php">options</a>.</p></div>';
+		}
+
+		function custom_login_wrong_settings() {
+			global $custom_login;
+
+			if ( $custom_login[ 'cl_login_hide_ad' ] != false )
+				echo '<div id="custom-login-warning" class="updated fade"><p><strong>You&prime;ve just hid the ad.</strong> Thanks for <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7329157" title="Donate on PayPal" class="external">donating</a>!</p></div>';
+		}
+
+		add_action( 'admin_notices', 'custom_login_warning' );
+		add_action( 'admin_notices', 'custom_login_wrong_settings' );
+
+return;
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,212 @@
+=== Custom Login ===
+Contributors: austyfrosty
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7431290
+Tags: admin, branding, customization, custom login, login, logo, error, login error
+Requires at least: 2.5
+Tested up to: 3.0
+Stable tag: 0.7.2
+
+Use this plugin to customize your login screen, great for client sites!
+
+== Description ==
+
+Visit the plugin page: [http://austinpassy.com//wordpress-plugins/custom-login](http://austinpassy.com//wordpress-plugins/custom-login)
+Visit my portfolio: [http://austinpassy.com//wordpress-plugins/custom-login](http://austinpassy.com//wordpress-plugins/custom-login)
+
+Activate this plugin and customize your WordPress login screen. Use the built-in and easy to use settings page to do the work for you. Theres no need to understand CSS at all!
+Now featureing a HTML &amp; CSS box for advanced users to up the customization!
+
+1. Works great for client site installs.
+2. Comes with a Photoshop template included in the library files (default theme).
+3. Read more about the [Custom Login Plugin](http://austinpassy.com/wordpress-plugins/custom-login/).
+
+**For those looking to showoff your login screen, check out the [Flickr group](http://flickr.com/groups/custom-login/)! Share you designs with the community!**
+
+== Installation ==
+
+Follow the steps below to install the plugin.
+
+1. Upload the `custom-login` directory to the /wp-content/plugins/ directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Settings/custom-login to edit your settings.
+4. **Be sure to *check* active** to remove the *default* login screen.
+
+
+== Frequently Asked Questions ==
+
+= Why create this plugin? =
+I created this plugin to allow for custom login of any WordPress login screen. See working example at: [WordCamp.LA](http://2010.wordcamp.la/wp-login.php?action=login).
+
+= Where can I upload and share my cool login screen? =
+Check out the newly created [Flickr group](http://flickr.com/groups/custom-login/)! Upload and add it to our pool!
+
+= I don't like the default login =
+Well you should check the first box `Use your own CSS` and customize **your** login screen!
+
+= I think i want to uninstall but =
+Just deactive.
+
+== Screenshots ==
+
+Screenshots of working example in our [Flickr group](http://flickr.com/groups/custom-login/)
+
+1. Custom Login Settings page (as of v 0.4.7).
+
+2. Example of a custom login page.
+
+== Changelog ==
+
+= Version 0.8 =
+
+* Rewrite of many features.
+* Removed unistall script.
+* Removed [wpads.net](http://wpads.net).
+* Cleaned up options.
+
+= Version 0.7.2 =
+
+* Fixed bug where plugin isn't allowed to be *deleted*(removed) when using WordPress 3.0+.
+
+= Version 0.7.1 =
+
+* Dashboard widget bug, upgrade mandatory.
+
+= Version 0.7 =
+
+* Updated `array_slice` error
+* Added `wp_wb_version` check for WordPress 3+
+* Added new option for WordPress 3+, *body background*.
+* Added check for version in animated error.
+** WP3 now includes the jQuery error
+* Now 100% compatible with WordPress 3.x
+
+= Version 0.6.1 =
+
+* Turned off the animated Autoresizer for expanding textareas, as it was buggy.
+
+= Version 0.6 =
+
+* Addded custom javascript error animation *turned on by default*
+* Cleaned up settings page
+
+= Version 0.5.2 =
+
+* Added dashboard widget.
+* Moved preview link higher.
+* Bug fix.
+
+= Version 0.5.1 =
+
+* Changes a function name.
+* Max character fix in `border-top-color`.
+
+= Version 0.5 =
+
+* **NEW** Javascript color picker [jscolor](http://jscolor.com)
+* **NEW** New field allows for colorization of the *body* `border-top` color
+* More options added
+* Cleaned up code
+* Testing new inline ad feature from [wpAds](http://bit.ly/wpadsnet) *Please leave [feedback/comments](http://austinpassy.com/2010/02/custom-login-version-0-5/) on this feature*
+
+= Version 0.4.8 =
+
+* Thickbox preview link :) *Let me know if you like the placement of it [in these comments](http://austinpassy.com/2010/02/custom-login-version-0-4-8/)*
+
+= Version 0.4.7 =
+
+* Added ability to use RGB/A colors to all color selectors. Max characters is now `21` instead of `7`
+* Cleaned up options page.
+* Added expanding textarea's for better coding space.
+* Allow for expanding help section per item basses.
+* Added an uninstaller (remove options from the database) *use the uninstall.php script*
+* **NEW** default style.
+
+= Version 0.4.6.1 =
+
+* CSS Bug.
+
+= Version 0.4.6 =
+
+* Added custom html coded box. Will only be in use if the box is checked.
+* New html box used jQuery to write to the page.
+
+= Version 0.4.5 =
+
+* Removed: #dock scroller (position fixed)
+* Added collapsing/expanding boxes on left to allow for visibility of color wheel.
+
+= Version 0.4.4.1 =
+
+* Error: Missed a period, caused fatal error.
+* Noticed issue with color picker error, try to reload page while I troubleshoot.
+* Fixed missing `div` tag on settings page.
+* Added two screenshots, one of settings page, one example.
+
+= Version 0.4.4 =
+
+* Added custom field box to add in your own CSS
+* Added in new toggle (hide the color box when you click on the `h3` title so as not to interfere when it auto scrolls)  
+
+= Version 0.4.3 =
+
+* Bug: When first installed, color fields need the `#` before the HEX numbers shows.
+
+= Version 0.4.2 =
+
+* Added an additional save button under the scrolling window dock when the window height is causing the window to jump.
+
+= Version 0.4.1.1 =
+
+* Bug fix: `Dock` is in the wrong div
+
+= Version 0.4.1 =
+
+* Added a `position:fixed` style to the color picker if the window scrolls below the view of the *Color picker*
+
+= Version 0.4 =
+
+* Added jQuery javascript color picker
+* Remeber to use the new color selections with **#** before the six `hex` keys!
+
+= Version 0.3.3 =
+
+* Added ability to have transparent background image for `html`
+* Added `html > background-repeat`
+
+= Version 0.3.2.1 =
+
+* Style: "Addded css style to `Delete/Reset` button"
+
+= Version 0.3.2 =
+
+* Bug: "if login form background color was empty, image wouldn't show"
+
+= Version 0.3.1 =
+
+* Bug: "login form backgound url" overwrote "login form backgound color"
+* Auto install into double directory
+
+= Version 0.3 =
+
+* Admin panel added
+* Additional CSS / user options
+* Custom login
+* WordPress 2.9 ready
+
+= Version 0.2 =
+
+* 2.7 CSS update.
+* readme.txt link updated.
+
+= Version 0.1 =
+
+* Initial release.
+
+
+== Upgrade Notice ==
+
+= 0.7.2 =
+Uninstall script no longer valid for WordPress 3.0+
+
+= 0.7.1 =
+Compatiblity issue with my other plugin [@Anywhere](http://austinpassy.com/anywhere-plugin) fixed.
