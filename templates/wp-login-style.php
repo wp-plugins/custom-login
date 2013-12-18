4014be5eb0f827c54db6c76e7453e2379d890455
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-18 18:26:45 +0000

    Version 2.1.9
    git-svn-id: https://plugins.svn.wordpress.org/custom-login/trunk@824886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/wp-login-style.php
+++ templates/wp-login-style.php
@@ -22,7 +22,10 @@ if ( false === ( $css = get_transient( $login->id . '_style' ) ) ) :
 	$css = '';
 	$close_rule = "}\n";
 	
-	if ( defined( 'WP_LOCAL_DEV' ) && WP_LOCAL_DEV ) $css .= "/**\n *\n" . print_r( $cl_css_atts, true ) . " */\n\n";
+	if ( defined( 'WP_LOCAL_DEV' ) && WP_LOCAL_DEV ) {
+		
+		$css .= "/**\n *\n" . print_r( $cl_css_atts, true ) . " */\n\n";
+	}
 		
 	$css .= "
 /**
@@ -186,7 +189,19 @@ if ( false === ( $css = get_transient( $login->id . '_style' ) ) ) :
 		
 			$css .= prefixit( 'background-size', $logo_background_size_custom );
 			
+		} else {
+			$css .= prefixit( 'background-size', 'inherit' );
 		}
+		
+		/* Get width & height from attachment image *
+		$attachment			= get_attached_file_id_from_url( $logo_background_url );
+		$attachment_id		= isset( $attachment[0] ) && !empty( $attachment[0] ) ? $attachment[0]->ID : $attachment;
+		$image_attributes 	= wp_get_attachment_image_src( $attachment_id ); // returns an array
+		$css .= !empty( $image_attributes[2] ) ? trailingsemicolonit( "height: {$image_attributes[2]}" ) : trailingsemicolonit( "height: inherit" );
+		$css .= !empty( $image_attributes[1] ) ? trailingsemicolonit( "width: {$image_attributes[1]}" ) : trailingsemicolonit( "width: inherit" );
+		 */
+		 $css .= !empty( $logo_background_height ) ? trailingsemicolonit( "height: {$logo_background_height}" ) : trailingsemicolonit( "height: inherit" );
+		 $css .= !empty( $logo_background_width ) ? trailingsemicolonit( "width: {$logo_background_width}" ) : trailingsemicolonit( "width: inherit" );
 		
 		/* CLOSE login h1 a */
 		$css .= $close_rule;
