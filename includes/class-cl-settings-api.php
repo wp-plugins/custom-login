d3bbbbbaed61885410707be1a1862fc3150bd65b
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-05 23:07:17 +0000

    Changing https:// to http links.
    git-svn-id: https://plugins.svn.wordpress.org/custom-login/trunk@1128412 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-cl-settings-api.php
+++ includes/class-cl-settings-api.php
@@ -774,14 +774,14 @@ class CL_Settings_API {
 			</a>', _x( 'Rate', 'rate; as in rate this plugin', $this->settings['domain'] ) );
 		
 		$content .= '<ul>';
-		$content .= sprintf( '<li>%s: <a href="https://austin.passy.co" target="_blank">Austin Passy</a></li>', _x( 'Author', 'the author of this plugin', $this->settings['domain'] ) );
+		$content .= sprintf( '<li>%s: <a href="http://austin.passy.co" target="_blank">Austin Passy</a></li>', _x( 'Author', 'the author of this plugin', $this->settings['domain'] ) );
 		$content .= sprintf( '<li>%s: <a href="https://twitter.com/TheFrosty" target="_blank">TheFrosty</a></li>', __( 'Twitter', $this->settings['domain'] ) );
 		$content .= '</ul>';
 		
 		$content .= sprintf( __( '<small>If you have suggestions for a new add-on, feel free to open a support request on <a href="%s" target="_blank">GitHub</a>. Want regular updates? Follow me on <a href="%s" target="_blank">Twitter</a> or visit my <a href="%s" target="_blank">blog</a>.</small>' ),
 			'https://github.com/thefrosty/custom-login/issues',
 			'https://twitter.com/TheFrosty',
-			'https://austin.passy.co'
+			'http://austin.passy.co'
 		);
 		
 		$this->postbox( 'frosty-media-author', __( 'Custom Login', $this->settings['domain'] ), $content );
